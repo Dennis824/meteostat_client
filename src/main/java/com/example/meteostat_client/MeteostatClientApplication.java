003136d20428582235c9f736f40209bec93990ad
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-07-24 15:46:32 -0600

    Initial commit

---
+++ src/main/java/com/example/meteostat_client/MeteostatClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.meteostat_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteostatClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteostatClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meteostat_client/MeteostatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meteostat_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteostatClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
